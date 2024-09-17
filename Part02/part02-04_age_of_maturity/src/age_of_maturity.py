bcc24b74fe730fea017f3bc29cc3dc89789fbed8
Cauã Santos <165220981+cauafsantosdev@users.noreply.github.com>
2024-09-16 22:43:31 -0300

    commiting exercises

---
+++ Part02/part02-04_age_of_maturity/src/age_of_maturity.py
@@ -0,0 +1,8 @@
+# Write your solution here
+
+age = int(input("What's your age? "))
+
+if age < 18:
+    print("You are not of age!")
+else:
+    print("You are of age!")
---
+++ Part02/part02-05_greater_or_equal/src/greater_or_equal.py
@@ -0,0 +1,11 @@
+# Write your solution here
+
+n1 = int(input("Number 1:"))
+n2 = int(input("Number 2: "))
+
+if n1 > n2:
+    print(f"The greater number was: {n1}")
+elif n2 > n1:
+    print(f"The greater number was: {n2}")
+else:
+    print("The numbers are equal!")
