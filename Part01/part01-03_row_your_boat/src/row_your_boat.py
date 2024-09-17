c04fcd54ca2a19a730db29a7a2f6d99287a69e23
Cauã Santos <165220981+cauafsantosdev@users.noreply.github.com>
2024-09-16 22:24:44 -0300

    commiting exercises

---
+++ Part01/part01-03_row_your_boat/src/row_your_boat.py
@@ -0,0 +1,3 @@
+# Write your solution here
+
+print(f"Row, row, row your boat,\nGently down the stream.\nMerrily, merrily, merrily, merrily,\nLife is but a dream.")
---
+++ Part01/part01-08_name_and_address/src/name_and_address.py
@@ -0,0 +1,7 @@
+# Write your solution here
+
+given = input("What's you given name? ")
+family = input("What's your family name? ")
+adress = input("What's your street address? ")
+city = input("What's your city and postal code? ")
+print(f"{given} {family}\n{adress}\n{city}")
